24b92706a078e86e333fcf7d50d2f5ac958231df
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-12 16:38:51 +0900

    시체 들고 내리기

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -3,42 +3,57 @@ using UnityEngine.InputSystem;
 
 public class Rope : MonoBehaviour
 {
+    public InputSystem_Actions action;
+    public float moveSpeed = 5f;
     public float climbSpeed = 3f;
-    private Rigidbody2D rb;
+    public LayerMask ropeLayer;
+    private Vector2 moveInput;
     private bool isOnRope = false;
-    private Vector2 ropeInput; 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        action = new InputSystem_Actions();
     }
-    public void OnRope(InputAction.CallbackContext ctx)
+
+    private void OnEnable()
     {
-        ropeInput = ctx.ReadValue<Vector2>();
+        //action.Enable();
     }
     // Update is called once per frame
     void Update()
     {
+        // 좌우 이동
+        transform.position += new Vector3(moveInput.x, 0, 0) * moveSpeed * Time.deltaTime;
+
+        // 로프 위/아래 이동
         if (isOnRope)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, ropeInput.y * climbSpeed);
-            rb.gravityScale = 0;
+            transform.position += new Vector3(0, moveInput.y, 0) * climbSpeed * Time.deltaTime;
+        }
+
+        // 레이캐스트로 로프 감지
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 0.5f, ropeLayer);
+        if (hit.collider != null)
+        {
+            isOnRope = true;
         }
         else
         {
-            rb.gravityScale = 1;
+            hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, ropeLayer);
+            isOnRope = hit.collider != null;
         }
     }
-    private void OnTriggerEnter2D(Collider2D col)
+    public void OnRope(InputAction.CallbackContext ctx)
     {
-        if (col.CompareTag("Rope")) isOnRope = true;
+        moveInput = ctx.ReadValue<Vector2>();
+        Debug.Log("onRope");
     }
-    private void OnTriggerExit2D(Collider2D col)
+
+    private void OnDrawGizmos()
     {
-        if (col.CompareTag("Rope")) isOnRope = false;
+        // 레이 시각화
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * 0.5f);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 0.5f);
     }
 }
